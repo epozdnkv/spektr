4a2a29c348d7044cf85cb1e0b9dc29d946412fa6
Evgeny Pozdnyakov <89064421+epozdnkv@users.noreply.github.com>
2022-06-08 20:55:51 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,3 +2,11 @@ window.onload = function(){
     document.body.scrollTop = 0;
 }
 if(location.hash) history.replaceState({}, document.title, location.href.split('#')[0]);
+
+const menuItems = document.querySelectorAll('.mmenu li');
+const menuToggle = document.querySelector('#menu-toggle');
+for(let i = 0; i < menuItems.length; i++){
+    menuItems[i].onclick = function(){
+        menuToggle.checked = false;
+    }
+}
